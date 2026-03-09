322880ff7812eb2e2b54c7045f81b98ff25a12e8
Gustavo Henrique Moreira Porto <gustavo.porto1052@gmail.com>
2026-03-09 16:30:51 -0300

    :tada: init: Primeiro commit

---
+++ fatecads/src/main/java/br/com/fatecads/fatecads/entity/Aluno.java
@@ -0,0 +1,34 @@
+package br.com.fatecads.fatecads.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+
+@Entity
+public class Aluno{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer idAluno;
+
+    @Column(nullable = false, length = 40)
+    private String nomeAluno;
+
+    @Column(length = 100)
+    private String emailAluno;
+
+    @Column(nullable = false, length = 11)
+    private String telefoneAluno;
+
+    @Column(nullable=false, length = 50)
+    private String enderecoAluno;
+
+    @Column(nullable = false, length = 11)
+    private String cpfAluno;
+
+    @Column(nullable = false, length = 100)
+    private String raAluno;
+
+
+}
